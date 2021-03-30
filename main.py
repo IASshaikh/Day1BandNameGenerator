1fe7ae7aa2c27a98fac2a38f0365b21fdf55ef3d
imran shaikh <imranshaikh.85@gmail.com>
2021-03-30 17:09:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("welcome to the band name Generator.")
+city = input("which city you born?")
+petname = input("what is your pet name?")
+print("your band name is" + " " + city + " " + petname)
