047d1b05dae97831a3144017511d813f4ba91e4c
imran shaikh <imranshaikh.85@gmail.com>
2021-03-30 17:24:33 +0000

    changed the print statement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("welcome to the band name Generator.")
 city = input("which city did you grow up in?")
 petname = input("what is your pet name?")
-print("your band name is" + " " + city + " " + petname)
+print("your band name could be" + " " + city + " " + petname)
