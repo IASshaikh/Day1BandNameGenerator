9d0632317d17666612a21f122424c2fd60d2805c
imran shaikh <imranshaikh.85@gmail.com>
2021-03-30 17:11:49 +0000

    input description

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("welcome to the band name Generator.")
-city = input("which city you born?")
+city = input("which city did you grow up in?")
 petname = input("what is your pet name?")
 print("your band name is" + " " + city + " " + petname)
